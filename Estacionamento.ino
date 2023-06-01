7b9b569d9b43d90cf1c46167db8e4d9e35c24473
João Henrique <126480950+Joaohnt27@users.noreply.github.com>
2023-05-31 23:02:25 -0300

    Add files via upload

---
+++ Estacionamento.ino
@@ -0,0 +1,65 @@
+// Arduino Car Parking System
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2);  //Change the HEX address
+#include <Servo.h> 
+
+Servo myservo1;
+
+int IR1 = 2;
+int IR2 = 4;
+
+int Slot = 4;           //Enter Total number of parking Slots
+
+int flag1 = 0;
+int flag2 = 0;
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+pinMode(IR1, INPUT);
+pinMode(IR2, INPUT);
+  
+myservo1.attach(3);
+myservo1.write(100);
+
+lcd.setCursor (0,0);
+lcd.print("     ARDUINO    ");
+lcd.setCursor (0,1);
+lcd.print(" ESTACIONAMENTO ");
+delay (2000);
+lcd.clear();  
+}
+
+void loop(){ 
+
+if(digitalRead (IR1) == LOW && flag1==0){
+if(Slot>0){flag1=1;
+if(flag2==0){myservo1.write(180); Slot = Slot-1;}
+}else{
+lcd.setCursor (0,0);
+lcd.print("    DESCULPE :(    ");  
+lcd.setCursor (0,1);
+lcd.print("  LOTADO!  "); 
+delay (3000);
+lcd.clear(); 
+}
+}
+
+if(digitalRead (IR2) == LOW && flag2==0){flag2=1;
+if(flag1==0){myservo1.write(180); Slot = Slot+1;}
+}
+
+if(flag1==1 && flag2==1){
+delay (1000);
+myservo1.write(100);
+flag1=0, flag2=0;
+}
+
+lcd.setCursor (0,0);
+lcd.print("    BEM-VINDO!    ");
+lcd.setCursor (0,1);
+lcd.print("Vagas: ");
+lcd.print(Slot);
+}
